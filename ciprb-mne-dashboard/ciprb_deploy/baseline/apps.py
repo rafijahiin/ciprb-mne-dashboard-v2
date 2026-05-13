05eb7ec791fe4368d61dd4f6d95b3cea93072b3c
rafijahiin <rafijahiin@gmail.com>
2026-05-14 05:11:45 +0600

    Delete ciprb-mne-dashboard directory

--- ciprb-mne-dashboard/ciprb_deploy/baseline/apps.py
+++
@@ -1,4 +0,0 @@
-from django.apps import AppConfig
-
-class BaselineConfig(AppConfig):
-    name = 'baseline'
--- ciprb-mne-dashboard/ciprb_deploy/mpdsr/apps.py
+++
@@ -1,4 +0,0 @@
-from django.apps import AppConfig
-
-class MpdsrConfig(AppConfig):
-    name = 'mpdsr'
